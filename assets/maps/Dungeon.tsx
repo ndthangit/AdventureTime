fa2ede2e5647e1e1df3aeb6ffa5ccc15bbf174f3
kien262004 <nkien6827@gmail.com>
2024-04-09 14:14:55 +0700

    fifth

---
+++ assets/maps/Dungeon.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Dungeon" tilewidth="16" tileheight="16" tilecount="48" columns="12">
+ <image source="../Backgrounds/Tilesets/TilesetDungeon.png" width="192" height="64"/>
+ <tile id="17">
+  <properties>
+   <property name="type" value="TRAP"/>
+  </properties>
+  <animation>
+   <frame tileid="16" duration="500"/>
+   <frame tileid="17" duration="500"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ assets/maps/item.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="item" tilewidth="16" tileheight="16" tilecount="90" columns="10">
+ <image source="../Items/AllPreview.png" width="160" height="156"/>
+</tileset>
