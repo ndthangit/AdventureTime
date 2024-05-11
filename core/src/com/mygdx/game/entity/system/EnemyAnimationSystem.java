4675f084de56e30b0fb9ea16b58287668aefa4cb
kien262004 <nkien6827@gmail.com>
2024-05-11 19:12:22 +0700

    20th

--- core/src/com/mygdx/game/entity/system/EnemyAnimationSystem.java
+++ core/src/com/mygdx/game/entity/system/EnemyAnimationSystem.java
@@ -1,4 +1,16 @@
 package com.mygdx.game.entity.system;
 
-public class EnemyAnimationSystem {
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+
+public class EnemyAnimationSystem extends IteratingSystem {
+    public EnemyAnimationSystem(Family family) {
+        super(family);
+    }
+
+    @Override
+    protected void processEntity(Entity entity, float v) {
+
+    }
 }
